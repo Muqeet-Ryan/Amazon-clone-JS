521c508c316cfd031a27666e66dab0e198d44a95
Muqeet-Ryan <muqeetryan@outlook.com>
2025-09-17 22:21:31 +0600

    added localStorage, added delivery ids

--- data/cart.js
+++ data/cart.js
@@ -1,8 +1,13 @@
 import { products } from "./products.js";
 
-export let cart = [
-  { productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6", quantity: 2 }
+export let cart = JSON.parse(localStorage.getItem('cart')) || [
+  { productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6", quantity: 2, deliveryOptionId: '3' }
 ]; ;
+
+  function saveToStorage(){
+    localStorage.setItem('cart', JSON.stringify(cart));
+  }
+
 
 export function addToCart(productId, quantity, container) {
   const matchingProduct = products.find((p) => productId === p.id);
@@ -15,6 +20,7 @@ export function addToCart(productId, quantity, container) {
     cartItem = {
       productId,
       quantity,
+      deliveryOptionId: '1'
     };
     cart.push(cartItem);
   }
@@ -23,4 +29,16 @@ export function addToCart(productId, quantity, container) {
   addedText.innerHTML = `<img src="images/icons/checkmark.png"> Added ${cartItem.quantity}`;
 
   console.log(cart);
+
+  saveToStorage();
+}
+
+export function updateDeliveryOption(productId, deliveryOptionId){
+
+  const matchingProduct = cart.find((c)=> c.productId === productId);
+
+  matchingProduct.deliveryOptionId = deliveryOptionId; 
+
+  saveToStorage();
+
 }
